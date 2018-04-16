cc3143bc6e357b345d310df90b463eba90205274
NSeroux <nico.seroux@gmail.com>
2018-04-16 12:07:51 +0200

    Modified Footer

--- footer.php
+++ footer.php
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 <footer>
     <p>Opera Omnia - 2018<br/>
     Pre Pre Alpha 0.0.1</p>
-=======
-<footer>
-    <p>Opera Omnia - 2018<br/>
-    Pre Pre Alpha 0.0.1</p>
->>>>>>> 692cc8af0adc95b649212ed7af4961f6b4c1b07e
 </footer>
