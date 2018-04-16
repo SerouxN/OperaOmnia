739486fd9753b5d217dff88ec650956a6c90ad50
NSeroux <nico.seroux@gmail.com>
2018-04-16 15:08:53 +0200

    Fixed footer links, and added wikisource

--- footer.php
+++ footer.php
@@ -7,9 +7,10 @@
             The different papers featured on this site come from:
                 <ul>
                     <li><a title="arXiv" href="https://arxiv.org/"><img src="images/arXivLogo.png" /></a></li>
-                    <li><a title="JSTOR" href="https://arxiv.org/"><img src="images/jstorLogo.png" /></a></li>
-                    <li><a title="Einstein Papers Archive" href="https://arxiv.org/"><img src="images/princetonLogo.png" /></a></li>
-                    <li><a title="The Euler Archive" href="https://arxiv.org/"><img src="images/eulerLogo.png" /></a></li>
+                    <li><a title="JSTOR" href="https://www.jstor.org/"><img src="images/jstorLogo.png" /></a></li>
+                    <li><a title="Wikisource" href="https://wikisource.org/"><img src="images/wikiLogo.png" /></a></li>
+                    <li><a title="Einstein Papers Archive" href="http://einsteinpapers.press.princeton.edu/"><img src="images/princetonLogo.png" /></a></li>
+                    <li><a title="The Euler Archive" href="http://eulerarchive.maa.org/"><img src="images/eulerLogo.png" /></a></li>
                 </ul>
         </p>
     </div>
