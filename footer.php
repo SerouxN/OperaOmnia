5b33f0b5d61f8bee14c8243e614cc840d786c3c8
NSeroux <nico.seroux@gmail.com>
2018-04-16 13:29:20 +0200

    Updated Footer to include sources

--- footer.php
+++ footer.php
@@ -1,4 +1,15 @@
 <footer>
     <p>Opera Omnia - 2018<br/>
     Pre Pre Alpha 0.0.1</p>
+    <div id="sources">
+        <p>
+            The different papers featured on this site come from:
+                <ul>
+                    <li><a title="arXiv" href="https://arxiv.org/"><img src="images/arXivLogo.jpg" /></a></li>
+                    <li><a title="JSTOR" href="https://arxiv.org/"><img src="images/jstorLogo.png" /></a></li>
+                    <li><a title="Einstein Papers Archive" href="https://arxiv.org/"><img src="images/princetonLogo.png" /></a></li>
+                    <li><a title="The Euler Archive" href="https://arxiv.org/"><img src="images/eulerLogo.png" /></a></li>
+                </ul>
+        </p>
+    </div>
 </footer>
