977d915f46630e70223dd6d6c22b4556e3d7e288
NSeroux <nico.seroux@gmail.com>
2018-04-16 12:08:28 +0200

    Modified Authors page

--- authors.php
+++ authors.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
 <html>
 <head>
     <title>Opera Omnia - Authors</title>
@@ -55,62 +53,4 @@
     </section>
 <?php include("footer.php"); ?>
 </body>
-=======
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Opera Omnia - Authors</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body> 
-    <?php include("header.php"); ?>
-    <section>
-    <h1>Authors</h1>
-    <form method="get" action="#">
-        <fieldset>
-        <legend>Sort by:</legend>
-        <select name="sorting" size="1">
-            <option value="Name">Name
-            <option value="FirstName">First Name 
-            <option value="Birthday">Birth Date
-            <option value="DateOfDeath">Death Date
-        </select>
-        <div id="order">
-            <input type="radio" name="order" value=" " checked>Ascending<br>
-            <input type="radio" name="order" value="DESC">Descending<br>
-        </div>
-            <input id="submitButton" type="submit" value="Submit">
-        </fieldset>
-    </form>
-    <ul>
-    <?php
-        $chosenMethod='Name';
-        if ($_GET['sorting'])
-        {
-            $chosenMethod=$_GET['sorting'];
-        }
-        try
-        {
-            $bdd = new PDO('mysql:host=localhost;dbname=opera omnia;charset=utf8', 'root', '');
-            $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }
-        catch(Exception $e)
-        {
-                die('Erreur : '.$e->getMessage());
-        }
-        $reponse = $bdd->query('SELECT * FROM authors ORDER BY ' . $chosenMethod ." ". $_GET['order']);
-
-        while ($data = $reponse->fetch())
-        {
-    ?>
-        <li><a href="author.php?authid=<?php echo $data['ID']?>"><p><?php echo $data['FirstName'] ." ". $data['Name']; ?></p></a></li>
-    <?php
-        }
-        $reponse->closeCursor();
-    ?>
-    </ul>
-    </section>
-<?php include("footer.php"); ?>
-</body>
->>>>>>> 692cc8af0adc95b649212ed7af4961f6b4c1b07e
 </html>
