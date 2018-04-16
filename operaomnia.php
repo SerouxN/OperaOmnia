845529a2bb5dccb9b23a4e1423f4b2e91888ac9d
NSeroux <nico.seroux@gmail.com>
2018-04-16 12:54:18 +0200

    Modified home page

--- operaomnia.php
+++ operaomnia.php
@@ -4,11 +4,16 @@
     <title>Opera Omnia</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body>
-    <?php include("header.php"); ?>
-    
-    <p>Hello World!</p>
-    <p>Welcome to Opera Omnia. That's it.</p>
-<?php include("footer.php"); ?>
+<header>
+	<?php include("header.php"); ?>
+</header>
+<body class ="home">
+	<div class = "homeBox">
+    <h1>Opera Omnia</h1>
+	<p>Welcome to Opera Omnia. An index for all the scientific papers.</p>
+	</div>
+	<div id="space"></div>
+	<?php //include("footer.php"); ?>
+
 </body>
 </html>
