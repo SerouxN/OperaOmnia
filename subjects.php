2a5f8ea9839c2a0572f0d8ccb65f057575d7f0a2
NSeroux <nico.seroux@gmail.com>
2018-04-16 14:03:19 +0200

    Modified subjects.php

--- subjects.php
+++ subjects.php
@@ -8,7 +8,26 @@
     <?php include("header.php"); ?>
     
     <h1>Subjects</h1>
-    <p>Welcome to Opera Omnia. That's it.</p>
+    <div id="menuSubject">
+    	<div class = "subjectPhysics"><strong>Physics</strong></div>
+    	<?php 
+    	try {
+    	 $bdd = new PDO('mysql:host=localhost;dbname=opera omnia;charset=utf8', 'root', '');
+    	}
+    	catch(Exception $e)
+        {
+           die('Erreur : '.$e->getMessage());
+        }
+        $reponse = 
+        $reponse = $bdd->query('SELECT Title,ID FROM papers');
+        while ($data = $reponse->fetch())
+        {
+    	echo ('<div class = "subjectsPapers"><ul> <li> <a href='.$data['ID'].'>'.$data['Title'].'</li> </ul></div>');
+        }
+        ?>
+
+    </div>
+
 <?php include("footer.php"); ?>
 </body>
 </html>
