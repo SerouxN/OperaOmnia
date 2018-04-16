e10fc86d175e3405bfedffc41fa4c3cea4e11e87
NSeroux <nico.seroux@gmail.com>
2018-04-16 13:47:05 +0200

    Updated Footer style

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
         <p>
             The different papers featured on this site come from:
                 <ul>
-                    <li><a title="arXiv" href="https://arxiv.org/"><img src="images/arXivLogo.jpg" /></a></li>
+                    <li><a title="arXiv" href="https://arxiv.org/"><img src="images/arXivLogo.png" /></a></li>
                     <li><a title="JSTOR" href="https://arxiv.org/"><img src="images/jstorLogo.png" /></a></li>
                     <li><a title="Einstein Papers Archive" href="https://arxiv.org/"><img src="images/princetonLogo.png" /></a></li>
                     <li><a title="The Euler Archive" href="https://arxiv.org/"><img src="images/eulerLogo.png" /></a></li>
