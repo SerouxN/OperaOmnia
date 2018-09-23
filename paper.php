d0ddc9fa3a1d78ac4fe872e90a99cd0d5e8e1829
NSeroux <nico.seroux@gmail.com>
2018-09-23 17:48:55 +0200

    Minor improvements

--- paper.php
+++ paper.php
@@ -22,6 +22,8 @@
             $reponse2 = $bdd->query('SELECT * FROM authors WHERE ID='. $AuthID);
             while ($data = $reponse2->fetch())
             {
+                $AuthFirstName=$data['FirstName'];
+                $AuthLastName=$data['LastName'];
                 $AuthField=$data['Field'];
                 if ($AuthField=="Mathematics")
                 {
@@ -45,73 +47,90 @@
     <body>   
         <?php include("header.php"); ?>
         <section>
-            <h1><?php echo $title?></h1>
-            <h2>Description</h2>
+            <h1><strong><?php echo $title ?></strong><?php echo" by ". $AuthFirstName." ".$AuthLastName;?></h1>
+            <h2>&nbsp;&nbsp;&nbsp;&nbsp;Description</h2>
             <p>&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $description ?></p>
-            <h2>Versions</h2>
-            <table id=
-            <?php 
-            if ($field==0)
-            {
-                echo "\"versionListMaths\"";
-            } 
-            elseif ($field==1)
-            {
-                echo "\"versionListCpsci\"";
-            }
-            elseif ($field==2)
-            {
-                echo "\"versionListPhys\"";
-            }?>
-             width="90%">
-                <tr>
-                    <th>Title</th>
-                    <th>Version</th>
-                    <th>Link</th>
-                </tr>
-                <?php
-                    include 'PDFInfo.php';
-                    foreach (glob("papers/". $_GET['id']."_*") as $filename) {
-                        $paper = new PDFInfo;
-                        $paper->load($filename);
-                        $fileTitle="";
-                        $fileTitle=$paper->title;
-                        $version=substr($filename, -5, 1);
-                ?>
-                <tr>
-                    <td id="paperTitle"><?php echo $fileTitle ?></td>
-                    <td id="version"><?php 
-                        if ($version ==0)
-                        {
-                            echo "Original Scan";
-                        }   
-                        elseif ($version ==1)
-                        {
-                            echo "English";
-                        }    
-                        elseif ($version ==2)
-                        {
-                            echo "German";
-                        } 
-                        elseif ($version ==3)
-                        {
-                            echo "French";
-                        } 
-                        elseif ($version ==4)
-                        {
-                            echo "Dutch";
-                        } 
-                        elseif ($version ==5)
-                        {
-                            echo "Latin";
-                        } 
-                    ?></td>
-                    <td><a  id="goLink" href=<?php echo $filename?>>Go</a></td>
-                </tr>
-                <?php
+            <h2>&nbsp;&nbsp;&nbsp;&nbsp;Versions</h2>
+            <?php
+                $numberOfVersions=0;
+                foreach (glob("papers/". $_GET['id']."_*") as $filename) 
+                {
+                    $numberOfVersions=$numberOfVersions +1;
                 }
-                ?>
-            </table>
+                if ($numberOfVersions==0)
+                {
+                    echo "This paper is not available on Opera Omnia!";
+                }
+                else
+                {
+                    echo "There are <strong>".$numberOfVersions."</strong> different versions of this paper available on <em>Opera Omnia</em>.";
+                    ?>
+                    <table width="90%" id=
+                    <?php 
+                        if ($field==0)
+                        {
+                            echo "\"versionListMaths\"";
+                        } 
+                        elseif ($field==1)
+                        {
+                            echo "\"versionListCpsci\"";
+                        }
+                        elseif ($field==2)
+                        {
+                            echo "\"versionListPhys\"";
+                        }
+                    ?>>
+                        <tr>
+                            <th>Title</th>
+                            <th>Version</th>
+                            <th>Link</th>
+                        </tr>
+                            <?php
+                                include 'PDFInfo.php';
+                                foreach (glob("papers/". $_GET['id']."_*") as $filename) 
+                                {
+                                    $paper = new PDFInfo;
+                                    $paper->load($filename);
+                                    $fileTitle="";
+                                    $fileTitle=$paper->title;
+                                    $version=substr($filename, -5, 1);?>
+                        <tr>
+                            <td id="paperTitle"><?php echo $fileTitle ?></td>
+                            <td id="version"><?php 
+                                if ($version ==0)
+                                {
+                                    echo "Original Scan";
+                                }   
+                                elseif ($version ==1)
+                                {
+                                    echo "English";
+                                }    
+                                elseif ($version ==2)
+                                {
+                                    echo "German";
+                                } 
+                                elseif ($version ==3)
+                                {
+                                    echo "French";
+                                } 
+                                elseif ($version ==4)
+                                {
+                                    echo "Dutch";
+                                } 
+                                elseif ($version ==5)
+                                {
+                                    echo "Latin";
+                                } 
+                            ?></td>
+                            <td><a  id="goLink" href=<?php echo $filename?>>Go</a></td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </table>
+            <?php
+            }
+            ?>
         </section>
         <?php include("footer.php"); ?>
     </body>
